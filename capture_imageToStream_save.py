93a64f79921a08eb8f589baacab5f673f7c43577
Nobuo Hayashi <haya.biz@gmail.com>
2017-04-09 00:21:55 +0900

    rotate 180

--- capture_imageToStream_save.py
+++ capture_imageToStream_save.py
@@ -13,6 +13,8 @@ my_file = open('my_image.jpg', 'wb')
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
+
+camera.rotation = 180
 camera.capture(my_file)
 # At this point my_file.flush() has been called, but the file has
 # not yet been closed
