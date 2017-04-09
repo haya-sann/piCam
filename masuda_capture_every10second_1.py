0f18973eddfa0a5e9dbde7b8ec198de7d7cc2bcc
Nobuo Hayashi <haya.biz@gmail.com>
2017-04-10 00:10:30 +0900

    fix syntax

--- masuda_capture_every10second_1.py
+++ masuda_capture_every10second_1.py
@@ -6,11 +6,11 @@ import os
 
 num=1
 while num < 10:
-    while true:
-        todaydetail=datetime.datetime.today()
-        if todaydetail.second%10==0:
+    while True:
+        todaydetail = datetime.datetime.today()
+        if todaydetail.second%10 == 0:
             os.system('python /home/pi/nas/datedjpeg.py')
             print todaydetail.second
-    num +=1
+    num += 1
 print "End"
 
