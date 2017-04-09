5272c9fa60424564f70d6efb379e7ee9543a4db1
Nobuo Hayashi <haya.biz@gmail.com>
2017-04-10 01:01:35 +0900

    Small change

--- datedjpeg.py
+++ datedjpeg.py
@@ -7,6 +7,6 @@ import datetime
 now = datetime.datetime.now()
 dir_path = '/home/pi/nas/photo/testProgram/piCam/datedphoto/'
 file_name = now.strftime('%Y%m%d%H%M%S')
-print "now.strftime is " + file_name
+print "File Name will be  " + file_name + ".jpg"
 picamera = picamera.PiCamera()
 picamera.capture(dir_path + file_name + '.jpg')
