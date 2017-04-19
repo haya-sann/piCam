2c5ca175c312744595a2e382d54be200533e7711
Nobuo Hayashi <haya.biz@gmail.com>
2017-04-19 13:40:40 +0900

    実行ディレクトリ直下の./captureディレクトリにテスト写真を保存するようにした。

--- test_vhflip.py
+++ test_vhflip.py
@@ -2,13 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import picamera
+import time
+import datetime
+import os
+
+now = datetime.datetime.now()
+dir_path = './capture'
+file_name = now.strftime('%H%M%S')
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+    os.chmod(dir_path, 0777)
+
 camera = picamera.PiCamera()
-camera.resolution = (1280, 720)
+camera.resolution = (1920, 1080)
 camera.vflip = True
 camera.hflip = False
 camera.brightness = 60
 camera.rotation = 0
-camera.start_preview()
+# camera.start_preview()
 
-camera.capture('/home/pi/nas/photo/image_test_masuda_hflip.jpg')
+camera.capture(dir_path + '/'+file_name+'.jpg')
 
