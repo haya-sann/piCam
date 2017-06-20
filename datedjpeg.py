c46677c6c3bc1a0e2bf3b12338d93d4b79640908
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-21 02:00:00 +0900

    syntax error step=1

--- datedjpeg.py
+++ datedjpeg.py
@@ -12,8 +12,8 @@ print "File Name will be  " + file_name + ".jpg"
 picamera = picamera.PiCamera()
 picamera.start_preview()
 # Camera warm-up time
-for i in range(0, 50, step=5):
+for i in range(0, 50, 5):
     time.sleep(2)
-    picamera.contrast = 30
+    picamera.contrast = i
     picamera.annotate_text = file_name + " Camera Contrast is : " + str(picamera.contrast)
     picamera.capture(dir_path + file_name + '.jpg')
