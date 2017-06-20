d66b5c2b0deba42ffe1a85320695abca747a90f8
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-21 01:53:42 +0900

    contrast changes 0 to 50 in stem 5

--- datedjpeg.py
+++ datedjpeg.py
@@ -12,7 +12,8 @@ print "File Name will be  " + file_name + ".jpg"
 picamera = picamera.PiCamera()
 picamera.start_preview()
 # Camera warm-up time
-time.sleep(2)
-picamera.contrast = 30
-picamera.annotate_text = file_name + " Camera Contrast is : " + str(picamera.contrast)
-picamera.capture(dir_path + file_name + '.jpg')
+for i in range(0, 50, step=5):
+    time.sleep(2)
+    picamera.contrast = 30
+    picamera.annotate_text = file_name + " Camera Contrast is : " + str(picamera.contrast)
+    picamera.capture(dir_path + file_name + '.jpg')
